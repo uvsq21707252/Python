b72b5c58b7da66c5e795b0176c91da0832a20d18
Raphael MAROUANI <raphael.marouani@dev.thegreenbow>
2023-05-22 16:37:42 +0200

    Classement des voitures en fonctions des clicks

--- Script.py
+++ Script.py
@@ -1,7 +1,34 @@
+import tkinter as tk
+
 marques = ['fiat', 'ferrari', 'mclaren', 'bmw', 'rolls-Royce', 'renault', 'lamborghini']
+preferences = {marque: 0 for marque in marques}
+buttons = {}
 
-marques_triees = sorted(marques, key=len)
+def update_preferences(marque):
+    preferences[marque] += 1
+    sort_buttons()
 
-print("Marques de voitures triées par ordre croissant de longueur de nom :")
-for marque in marques_triees:
-    print(marque)
+def sort_buttons():
+    sorted_preferences = sorted(preferences.items(), key=lambda x: x[1], reverse=True)
+    for i, (marque, _) in enumerate(sorted_preferences):
+        buttons[marque].pack_forget()
+        buttons[marque].pack(side=tk.TOP, pady=5)
+
+def show_preferences():
+    sorted_preferences = sorted(preferences.items(), key=lambda x: x[1], reverse=True)
+    print("Classement des marques de voitures par ordre de préférence :")
+    for marque, count in sorted_preferences:
+        print(f"{marque}: {count} clics")
+
+root = tk.Tk()
+root.title("Classement des marques de voitures")
+
+for marque in marques:
+    button = tk.Button(root, text=marque, command=lambda marque=marque: update_preferences(marque))
+    buttons[marque] = button
+    button.pack(side=tk.TOP, pady=5)
+
+show_preferences_button = tk.Button(root, text="Afficher le classement", command=show_preferences)
+show_preferences_button.pack()
+
+root.mainloop()
